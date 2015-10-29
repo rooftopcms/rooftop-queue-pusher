d89adc32cdb94484c1200fc4ca42bdaf0b38850b
Paul Hendrick <paul@errorstudio.co.uk>
2015-10-29 17:40:58 +0000

    fork 2 queue runners by default

--- queues/runners/settings.php
+++ queues/runners/settings.php
@@ -4,7 +4,7 @@ require_once "../../../../../../vendor/autoload.php";
 $settings = [
     'REDIS_BACKEND'     => '127.0.0.1:6379',    // Set Redis Backend Info
     'REDIS_BACKEND_DB'  => '0',                 // Use Redis DB 0
-    'COUNT'             => '1',                 // Run 1 worker
+    'COUNT'             => '2',                 // Run 1 worker
     'INTERVAL'          => '5',                 // Run every 5 seconds
     'QUEUE'             => '*',                 // Look in all queues
     'PREFIX'            => 'rooftop',           // Prefix queues with rooftop
