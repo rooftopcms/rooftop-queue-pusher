e4aa93ad8c8ce69f4da0ac5424c6dc857a688518
Ed Jones <ed@errorstudio.co.uk>
2016-01-05 14:57:41 +0000

    Fix up redis host / port

--- queues/runners/settings.php
+++ queues/runners/settings.php
@@ -1,10 +1,22 @@
 <?php
 require_once "../../../../../../vendor/autoload.php";
+/**
+ * Use Dotenv to set required environment variables and load .env file in root
+ */
+$dotenv = new Dotenv\Dotenv('../../../../../../');
+if (file_exists('../../../../../../.env')) {
+    $dotenv->load();
+    $dotenv->required( [
+        'REDIS_HOST',
+        'REDIS_PORT',
+        'REDIS_DB'
+    ] );
+}
 
 $settings = [
-    'REDIS_BACKEND'     => REDIS_HOST.":".REDIS_PORT,    // Set Redis Backend Info
-    'REDIS_BACKEND_DB'  => REDIS_DB,                 // Use Redis DB 0
-    'COUNT'             => '2',                 // Run 1 worker
+    'REDIS_BACKEND'     => getenv('REDIS_HOST').":".getenv('REDIS_PORT'),    // Set Redis Backend Info
+    'REDIS_BACKEND_DB'  => getenv('REDIS_DB'),                 // Use Redis DB 0
+    'COUNT'             => '1',                 // Run 1 worker
     'INTERVAL'          => '5',                 // Run every 5 seconds
     'QUEUE'             => '*',                 // Look in all queues
     'PREFIX'            => 'rooftop',           // Prefix queues with rooftop
