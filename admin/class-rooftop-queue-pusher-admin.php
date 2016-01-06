07796413bd5e6e5b45fc2c716dca3e50124756d4
Ed Jones <ed@errorstudio.co.uk>
2016-01-06 10:19:56 +0000

    Include redis password

--- admin/class-rooftop-queue-pusher-admin.php
+++ admin/class-rooftop-queue-pusher-admin.php
@@ -57,6 +57,7 @@ class Rooftop_Queue_Pusher_Admin {
             'scheme' => 'tcp',
             'host'   => REDIS_HOST,
             'port'   => REDIS_PORT,
+            'password' => REDIS_PASSWORD
         ] );
 
         $this->blog_id = get_current_blog_id();
