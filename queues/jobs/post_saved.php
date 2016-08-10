7dfeca81d2086f7d03bf40302b1eb93a6c3bb428
Paul Hendrick <paul@errorstudio.co.uk>
2016-08-10 16:48:44 +0100

    refactor from curl to socket request. allows us to fire and forget the webhooks, but as a result we aren't re-scheduling based on endpoint response

--- queues/jobs/post_saved.php
+++ queues/jobs/post_saved.php
@@ -3,72 +3,32 @@ require_once(dirname(__FILE__).'/../../../../../../config/application.php');
 require_once('rooftop_job.php');
 
 class PostSaved extends RooftopJob {
-    private $retry_max = 3;
-
-    function setUp() {
-        $this->mysql = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-    }
     function tearDown() {
-        echo "\n\ntearDown\n\n";
-        $this->mysql->close();
-        unset($this->mysql);
     }
 
     public function perform() {
         echo "\n\nPerforming job...\n";
-        $inflector = ICanBoogie\Inflector::get('en');
 
         try {
             $payload = $this->args;
-            $url = $payload['endpoint']['url'];
             $body = $payload['body'];
 
-            $ch = curl_init($url);
-            curl_setopt($ch, CURLOPT_POST, 1);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_exec($ch);
-            $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            $url = parse_url( $payload['endpoint']['url'] );
+            $host = $url['host'];
+            $port = array_key_exists( 'port', $url ) ? $url['port'] : 80;
 
-            // if we don't have success with response, or success with no response - re-queue this message
-            $attempt = array_key_exists('attempts', $body) ? $body['attempts']+=1 : 1;
+            $request_socket = fsockopen( $host, $port, $errno, $errstr, 10 );
+            $request_body = "POST " . $url['path'] . " HTTP/1.1\r\n";
+            $request_body.= "Host: " . $url['host'] . "\r\n";
+            $request_body.= "Content-Type: application/x-www-form-urlencoded\r\n";
+            $request_body.= "Content-Length: " . strlen( http_build_query( $body ) ) . "\r\n";
+            $request_body.= "Connection: Close\r\n\r\n";
+            $request_body.= http_build_query( $body );
 
-            if( 200 !== $status && $attempt < $this->retry_max ) {
-                $payload['body']['attempts'] = $attempt;
-
-                $delay = (60 * $attempt * $attempt)*$attempt; // retry after 1, 8, 27 minutes before giving up
-                error_log("RETRY (status $status) - ". $this->retry_max . " > $attempt - retry in $delay");
-
-                ResqueScheduler::enqueueIn($delay, "content", "PostSaved", $payload);
-
-                $this->saveJobStatus($status, "Job failed at $attempt attempt. Will retry...");
-            }else {
-                error_log("Not re-running job. Status: $status reached after $attempt attempts.");
-                $this->saveJobStatus($status, "Job completed after $attempt attempts.");
-            }
+            fwrite( $request_socket, $request_body );
+            fclose( $request_socket );
         }catch (Exception $e) {
-            error_log("Not re-running job. Exception received: '" . $e->getMessage() . "'");
-            $this->saveJobStatus(500, "Job failed. Not retrying.");
-        }
-    }
-
-    private function saveJobStatus($status, $message) {
-        try {
-            $payload = $this->args;
-            $prefix = $table_prefix = getenv('DB_PREFIX') ?: 'wp_';
-            $table_name = $prefix . $payload['body']['blog_id']."_completed_jobs";
-            $rooftop_job_id = $this->job->payload['args'][0]['rooftop_job_id'];
-
-            $message = $this->mysql->real_escape_string($message);
-
-            $sql = <<<EOSQL
-UPDATE $table_name SET status = $status, message = '$message' WHERE rooftop_job_id = '$rooftop_job_id';
-EOSQL;
-            echo $sql . "\n\n";
-
-            $this->mysql->query($sql);
-        }catch (Exception $e) {
-
+            error_log("Exception raised: '" . $e->getMessage() . "'");
         }
     }
 }
